fecca48a1dab14f4574487850cd07e4cdd99ece4
gespana26 <151874827+gespana26@users.noreply.github.com>
2024-06-20 01:33:25 +0000

    Pending changes exported from your codespace

--- exercises/15-Looping-With-FizzBuzz/app.py
+++ exercises/15-Looping-With-FizzBuzz/app.py
@@ -1,5 +1,18 @@
 def fizz_buzz():
     # ✅↓ Write your code here ↓✅
+    j = 0 
+    for i in range(100):
+        j += 1
+        if (j % 3 == 0) and (j % 5 == 0):
+             print("FizzBuzz")
+        elif j % 3 == 0:
+            print("Fizz")
+        elif j % 5 == 0:
+            print("Buzz")
+        else :
+            print(j)
+        
+
 
 # ❌↓ DON'T CHANGE THE CODE BELOW ↓❌
 fizz_buzz()
